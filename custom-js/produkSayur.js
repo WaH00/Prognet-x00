3b9beb95f11ab1a3d2c52dd42ce5d6d6b7442b2e
Jayncuk <dhananjayaad24@gmail.com>
2022-10-27 00:44:34 +0800

    Ngga Ada Yang Penting

---
+++ custom-js/produkSayur.js
@@ -0,0 +1,9 @@
+function test(){
+    
+};
+
+function imgLoad(){
+    // ngambil alamat foto dari produk yang di click (gagal)
+    // getStorageImg = sessionStorage.getItem('imgSayur1');
+    // document.getElementById("singleProdukItem").src =   
+};
